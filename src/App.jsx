e8b77f9200c3f259d3eba0a4ee73fb601b741004
Marco Rava <marco.rava94@gmail.com>
2022-10-01 18:33:08 +0200

    added model

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { HelmetScene } from './components/HelmetScene';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HelmetScene />
+    </div>
+  );
+}
+
+export default App;
