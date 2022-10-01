5208018ac0a569f0b57121c2691d1084248b1335
Marco Rava <marco.rava94@gmail.com>
2022-10-01 20:12:51 +0200

    refactor SocialLinks

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,30 +1,19 @@
 import { LinkButton } from "../../components";
-import twitterLogo from "../../assets/icons/twitter-logo.svg";
-import dribbbleLogo from "../../assets/icons/dribbble-logo.svg";
-import youtubeLogo from "../../assets/icons/youtube-logo.svg";
+import { socialData } from "../../data";
 import "./SocialLinks.scss";
 
 export const SocialLinks = () => {
   return (
     <div className="socialLinks">
-      <LinkButton
-        bgColor="#1DA1F2"
-        icon={twitterLogo}
-        alt="Twitter Logo"
-        href="https://twitter.com/"
-      />
-      <LinkButton
-        bgColor="#FF0000"
-        icon={youtubeLogo}
-        alt="Youtube Logo"
-        href="https://youtube.com/"
-      />
-      <LinkButton
-        bgColor="#EA6493"
-        icon={dribbbleLogo}
-        alt="Dribbble Logo"
-        href="https://dribbble.com/"
-      />
+      {socialData.map((social) => (
+        <LinkButton
+          bgColor={social.bgColor}
+          icon={social.icon}
+          alt={social.alt}
+          href={social.href}
+          key={social.alt}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,24 @@
+import twitterLogo from "./assets/icons/twitter-logo.svg";
+import dribbbleLogo from "./assets/icons/dribbble-logo.svg";
+import youtubeLogo from "./assets/icons/youtube-logo.svg";
+
+export const socialData = [
+  {
+    bgColor: "#1DA1F2",
+    icon: twitterLogo,
+    alt: "Twitter Logo",
+    href: "https://twitter.com/",
+  },
+  {
+    bgColor: "#FF0000",
+    icon: youtubeLogo,
+    alt: "Youtube Logo",
+    href: "https://youtube.com/",
+  },
+  {
+    bgColor: "#EA6493",
+    icon: dribbbleLogo,
+    alt: "Dribbble Logo",
+    href: "https://dribbble.com/",
+  },
+];
