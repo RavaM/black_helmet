67f85320b5c2afb7261479a1d7ed5dfca3399eb4
Marco Rava <marco.rava94@gmail.com>
2022-10-02 17:14:15 +0200

    fixed text on scroll

--- src/components/ScrollText/ScrollText.jsx
+++ src/components/ScrollText/ScrollText.jsx
@@ -1,15 +1,20 @@
 import { useScroll } from "framer-motion";
+import { useEffect, useState } from "react";
 import { scrollData } from "../../data";
 import "./ScrollText.scss";
 
 export const ScrollText = () => {
   const { scrollYProgress } = useScroll();
+  const [yProgress, setYProgress] = useState(scrollYProgress.current);
+
+  useEffect(() => {
+    return scrollYProgress.onChange((latest) => {
+      setYProgress(latest);
+    });
+  }, [scrollYProgress]);
 
   const showText = (data) => {
-    return (
-      data.start <= scrollYProgress.current &&
-      data.end >= scrollYProgress.current
-    );
+    return data.start <= yProgress && data.end >= yProgress;
   };
 
   return (
