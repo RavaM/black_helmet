9bc823b87d876fa7d537405a0b4e0e971a089837
Marco Rava <marco.rava94@gmail.com>
2022-10-02 18:31:48 +0200

    added font

--- src/data.js
+++ src/data.js
@@ -25,24 +25,24 @@ export const socialData = [
 
 export const scrollData = [
   {
-    text: "ScrollText 1",
+    text: "Night visor",
     start: 0.1,
     end: 0.25,
     top: "23%",
     left: "68%",
   },
   {
-    text: "ScrollText 2",
+    text: "Air filler",
     start: 0.35,
     end: 0.5,
     top: "78%",
     left: "27%",
   },
   {
-    text: "ScrollText 3",
-    start: 0.6,
-    end: 0.75,
-    top: "37%",
+    text: "Custom open",
+    start: 0.63,
+    end: 0.8,
+    top: "32%",
     left: "17%",
   },
 ];
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import "./reset.scss";
+import "./index.scss";
 import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
 
